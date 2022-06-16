d0310a360f2edfbc835520a98392977bb0bd61d9
JichouP <akkunn100@gmail.com>
2022-06-16 12:03:46 +0900

    :white_check_mark: skip console.error when test

--- src/controllers/internalServerError.ts
+++ src/controllers/internalServerError.ts
@@ -7,7 +7,7 @@ const internalServerError: ErrorRequestHandler = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next
 ) => {
-  console.error(err);
+  if (process.env.NODE_ENV !== 'test') console.error(err);
   res.status(500);
   res.send('Internal Server Error');
 };
